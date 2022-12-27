eb1f8a0a8818ae0b172e1c05ed88ee12e094017c
kibethh <kibethh@gmail.com>
2022-12-27 11:40:45 +0300

    to add linting

---
+++ src/index.ts
@@ -0,0 +1,8 @@
+import express from "express";
+const app = express();
+const port = 9000;
+
+app.get("/", (req, res) => res.send("Hello, world!"));
+app.listen(port, () => {
+  console.log(`Server running on ${port}`);
+});
