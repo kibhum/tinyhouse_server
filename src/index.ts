70be40bbee454b14910400230ece1c86ed5e2cfc
kibethh <kibethh@gmail.com>
2022-12-28 11:05:24 +0300

    used graphql language

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,9 @@
 import express from "express";
 import { ApolloServer } from "apollo-server-express";
-import { schema } from "./graphql";
-
+import { typeDefs, resolvers } from "./graphql";
 const app = express();
 const port = 9000;
-const server = new ApolloServer({ schema });
+const server = new ApolloServer({ typeDefs, resolvers });
 (async () => {
   await server.start();
   server.applyMiddleware({ app, path: "/api" });
