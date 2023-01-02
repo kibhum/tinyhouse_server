0b74d8f74535622affa257badd6d9672cf495c94
kibethh <kibethh@gmail.com>
2023-01-02 11:41:26 +0000

    added authorize func

---
+++ src/lib/utils/index.ts
@@ -0,0 +1,7 @@
+import { Request } from "express";
+import { Database, User } from "../types";
+export const authorize = (db: Database, req: Request): Promise<User | null> => {
+  const token = req.get("X-CSRF-Token");
+  const viewer = db.users.findOne({ _id: req.signedCookies.viewer, token });
+  return viewer;
+};
